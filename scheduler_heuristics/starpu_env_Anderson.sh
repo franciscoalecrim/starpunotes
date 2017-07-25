95a2b8b3d86dc2696d883d3575785700c8605bc9
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-07-25 10:55:24 -0300

    Adicionado arquivo de configuração das variáveis de ambiente do stapu. Lembrar de usar o comando source, na hora de chamar o script

---
+++ scheduler_heuristics/starpu_env_Anderson.sh
@@ -0,0 +1,10 @@
+export STARPU_PATH=$HOME/starpu
+export STARPU_CONFIG_FOLDER="config_cuda" 
+export STARPU_PATH_AUDIO_FFT_EXAMPLES=$HOME/Documentos/svn/StarPU/examples/audio
+export PKG_CONFIG_PATH=$STARPU_PATH/lib/pkgconfig
+export LD_LIBRARY_PATH=$STARPU_PATH/lib
+export STARPU_EXAMPLES_DIR=$STARPU_PATH/lib/starpu/examples
+export STARPU_NCPU=1
+export STARPU_NCUDA=0
+export STARPU_OPENCL=1
+
